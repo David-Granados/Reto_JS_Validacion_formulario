1013b721d7fa6f1c310eb6e0636aad83100ba452
David Granados <73331579+David-Granados@users.noreply.github.com>
2022-10-17 19:16:11 +0200

    first change for update

--- js/validator.js
+++ js/validator.js
@@ -1,123 +1,85 @@
+const regexp_name = /^[a-zA-Z]+[a-zA-Z]+$/;
+const regexp_telf = /^[09][0-9]{1,7}$/;
+const regexp_mail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+const regexp_dni = /^\d{8}[a-zA-Z]$/;
+const regexp_iban = /[a-zA-Z]{2}[0-9]{20}$/;
+const regexp_username = /^[A-Za-z]\\w{5,29}$/;
 /**
  * Alerts
  */
- function myAlert(msg){
+function myAlert(msg){
     var div = document.createElement("div");
-    div.style.background="grey";
-    div.style.color="white";
-    div.style.textAlign="center";
+    div.style.background = "grey";
+    div.style.color = "white";
+    div.style.textAlign = "center";
     div.classList.add("alert");
-    div.innerHTML=msg;
+    div.innerHTML = msg;
     var close = document.createElement("span");
-    close.style.float="right";
-    close.style.marginRight="2%";
+    close.style.float = "right";
+    close.style.marginRight = "2%";
     close.classList.add("close");
-    close.innerHTML="X";
+    close.innerHTML = "X";
     div.appendChild(close);
-    $("body").insertBefore(div,$("body").firstChild);
+    $("body").insertBefore(div, $("body").firstChild);
     bind_close();
 }
 /**
  * Create alert and selector element
  */
-function $(selector) {
+function $(selector){
     return document.querySelector(selector);
 }
-const expresion_regular_name = /^[a-zA-Z]+[a-zA-Z]+$/;
-const expresion_regular_telf = /^[09][0-9]{1,7}$/;
-const expresion_regular_mail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-const expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
-const expresion_regular_iban = /[a-zA-Z]{2}[0-9]{20}$/;
-const expresion_regular_username = /^[A-Za-z]\\w{5,29}$/;
-
-$("#btn").addEventListener("click", (e) => {
-    e.preventDefault();
-    //Firstname
+//Validadores
+function validatorFirstname(){
     const firstname = document.getElementById("firstname").value;
-    if (!firstname) {
-        myAlert("Campo nombre vacío");
-    } else {
-        if (!expresion_regular_name.test(firstname)) {
-            myAlert("Nombre erróneo. Formato no válido");
-        }
+    if (!expresion_regular_name.match(regexp_name)){
+        myAlert("Nombre erróneo. Formato no válido");
     }
-    //Surname
+}
+function validatorSurname(){
     const surname = document.getElementById("surnames").value;
-    if (!surname) {
-        myAlert("Campo apellidos vacío");
-    } else {
-        if (!expresion_regular_name.test(surname)) {
-            myAlert("Apellidos erróneos. Formato no válido");
-        }
+    if (!expresion_regular_name.match(regexp_name)){
+        myAlert("Apellidos erróneos. Formato no válido");
     }
-    //Mail
+}
+function validatorEmail() {
     const mail = document.getElementById("mail").value;
-    if (!mail) {
-        myAlert("Campo Email vacío");
-    } else {
-        if (!expresion_regular_mail.test(mail)) {
-            myAlert("Formato de Email no valido");
-        }
+    if (!expresion_regular_mail.match(regexp_mail)){
+        myAlert("Formato de Email no valido (alguien@dominio.org)");
     }
+}
+function validatorTelephone(){
     //Telephone
     const telephone = document.getElementById("telf").value;
-    if (!telephone) {
-        myAlert("Campo teléfono vacío");
-    }else{
-        if(!expresion_regular_telf.test(telephone)){
-            myAlert("Formato de telefono no válido");
-        }
+    if (!expresion_regular_telf.match(regexp_telf)){
+        myAlert("Formato de telefono no válido (+34600000000)");
     }
-    //DNI
+}
+function validaroDNI(){
     const dni = document.getElementById("dni").value;
-    if (expresion_regular_dni.test(dni)) {
+    if (expresion_regular_dni.match(regexp_dni)) {
         numero = dni.substr(0, dni.length - 1);
         letr = dni.substr(dni.length - 1, 1);
         numero = numero % 23;
         letra = 'TRWAGMYFPDXBNJZSQVHLCKET';
         letra = letra.substring(numero, numero + 1);
-        if (letra != letr.toUpperCase()) {
+        if (letra != letr.toUpperCase()){
             myAlert('Dni erroneo, la letra del NIF no se corresponde');
-        } else {
-            myAlert('Dni correcto');
+
         }
-    } else {
-        if (dni) {
-            myAlert('Dni erroneo, formato no válido');
-        } else {
-            myAlert('Campo DNI vacío');
-        }
-    }
-    //IBAN
-    const iban = document.getElementById("iban").value;
-    if (iban) {
-        if (!expresion_regular_iban.test(iban)) {
-            myAlert("IBAN erroneo, formato no válido");
-        }
-    } else {
-        myAlert("Campo IBAN vacío");
-    }
-    //Swift
-    const swift = document.getElementById("swift").value;
-    if (!swift) {
-        myAlert("Campo swift vacío");
     }else{
-        if(!expresion_regular_swift.test(swift)){
-            myAlert("Swift erroneo, formato no válido");
-        }
+       myAlert('Dni erroneo, formato no válido');
     }
-    //User name
+}
+function validatorIBAN(){
+    
+}
+function validatorSwift(){
+    
+}
+function validorUserName(){
     const username = document.getElementById("username").value;
-    if (!username) {
-        myAlert("Campo Nombre de Usuario vacío");
-    } else {
-        if (!expresion_regular_username.test(username)) {
-            myAlert("Nombre de usuario erroneo, formato no válido");
-        }
+    if (!expresion_regular_username.match(regexp_username)) {
+        myAlert("Nombre de usuario erroneo, formato no válido");
     }
-    //Birthday
-    const birthday = document.getElementById("birthday").value;
-    if (!birthday) {
-        myAlert("Campo Fecha de nacimiento vacío");
-    }
-});
+}
